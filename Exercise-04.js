e8e2f6e6008857df37e638d134b1676351e22714
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-01-17 14:49:26 +0700

    Luyen tap tao va su dung ham 01 - Bai 4

---
+++ Exercise-04.js
@@ -0,0 +1,12 @@
+result = 0;
+alert("result = " + result);
+
+function addNumbers() {
+    firstNum = 4;
+    secondNum = 8;
+    result = firstNum + secondNum;
+    alert("result = " + result);
+    return result;
+}
+
+result = addNumbers();
